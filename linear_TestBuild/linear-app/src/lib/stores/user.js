859ce3ed97a0126b89cbb14d9111c4c4d6a0705a
TheInfernoDragon <theinfernodragon2094@gmail.com>
2025-12-18 18:38:54 +0800

    Corrected the sytax error stuff

--- linear_TestBuild/linear-app/src/lib/stores/user.js
+++ linear_TestBuild/linear-app/src/lib/stores/user.js
@@ -20,8 +20,8 @@ export function setUser(userData) {
 		userId: userData.userId,
 		userName: userData.userName,
 		userEmail: userData.userEmail,
-		userAvatar: userData.userAvatar || ''
-		role: userData.role || 'user';
+		userAvatar: userData.userAvatar || '',
+		role: userData.role || 'user'
 	})
 
 	isLoggedIn.set(true);
@@ -32,7 +32,7 @@ export function setUser(userData) {
 export function clearUser(){
 	user.set({
 		userId: null,
-		userName: '';
+		userName: '',
 		userEmail: '',
 		userAvatar: '',
 		role: 'user'
