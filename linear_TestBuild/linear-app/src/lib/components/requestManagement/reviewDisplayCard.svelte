d1efd549b28464d97df7e112cc0fe9ca14c63c3b
luke-for-professional-help <kazefocalors@gmail.com>
2025-12-18 15:40:54 +0800

    Updated reviewDisplayCard

--- linear_TestBuild/linear-app/src/lib/components/requestManagement/reviewDisplayCard.svelte
+++ linear_TestBuild/linear-app/src/lib/components/requestManagement/reviewDisplayCard.svelte
@@ -5,8 +5,27 @@
     let { review } = $props(); 
 </script>
 
+<<<<<<< HEAD
+    let formModal = $state(false);
+    //placeholder values below
+    //server will load the variables for the following fields
+    // redview_id
+    //professor_name
+    //subject_selected //this means which subject is being selected + the professor that the user inputted in the "make review" page
+    //
+    let reviewId = 200089;
+    let professorName = "John Doe";
+    let subjectSelected = "CIS1101";
+
+    let review: string ="PLACE HODLER REVIEW FOR THIS STUPID ASS BALD ASS PROFESSOR"
+</script>   
+
+<Card class="h-30 max-w-full mb-3 mt-3 sm:p-5"> 
+    <div class="flex flex-row text-justify">
+=======
 <Card size="xl" class="mb-4">
     <div class="flex justify-between items-start">
+>>>>>>> c23756fb3feb0d739a9d9111c8171510050a7b82
         <div>
             <h5 class="text-lg font-bold">Review for {review.Professor_Name}</h5>
             <p class="text-sm text-gray-500">By {review.Username} | {review.Subject_Name}</p>
