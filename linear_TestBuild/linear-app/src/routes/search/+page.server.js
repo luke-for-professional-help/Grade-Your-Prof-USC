c12340b3eaa6c3e41e066a0883eccfc3f58eecd0
TheInfernoDragon <theinfernodragon2094@gmail.com>
2025-12-19 01:00:28 +0800

    saved page.server.js

---
+++ linear_TestBuild/linear-app/src/routes/search/+page.server.js
@@ -0,0 +1,15 @@
+import pool from '$lib/server/dbconnect';
+import { getSearchResults}  from "$lib/server/dbconnect.js";
+
+export async function load() {
+    try {
+        // Use the pool to query
+        const [rows] = await getSearchResults(query);
+        return {
+            results: rows
+        };
+    } catch (error) {
+        console.error("Database error:", error);
+        return { results: [], error: "Could not fetch data" };
+    }
+}
