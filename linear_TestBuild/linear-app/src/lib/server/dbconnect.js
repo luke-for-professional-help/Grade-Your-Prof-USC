9c468b40e7d70871027c4043a616290cac524d0a
TheInfernoDragon <theinfernodragon2094@gmail.com>
2025-12-27 17:21:31 +0800

    updated database credentials to match database in dcism.org

--- linear_TestBuild/linear-app/src/lib/server/dbconnect.js
+++ linear_TestBuild/linear-app/src/lib/server/dbconnect.js
@@ -4,9 +4,9 @@ import { error } from '@sveltejs/kit';
 
 const pool = mysql.createPool({
 	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'gradeyourprof',
+	user: 's31200214_gradeyourprof',
+	password: 'Gradepr0f##',
+	database: 's31200214_gradeyourprof',
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0
